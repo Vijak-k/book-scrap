44d260fda8bd31305d598692617193f79447645b
Vijak-k <vijak_k@hotmail.com>
2026-03-23 21:37:33 +0700

    update dbt: int_prod_detailes.sql

--- book_scrap_dbt/models/intermediate/int_prod_details.sql
+++ book_scrap_dbt/models/intermediate/int_prod_details.sql
@@ -1,14 +1,14 @@
 WITH staging AS (
     SELECT * FROM {{ ref('stg_prod_details') }}
 ),
-extracted AS (
+clean_title_1 AS (
     SELECT
         product_id,
-        TRIM(split_part(keywords, ',', 1)) AS first_keyword,
+        TRIM(split_part(keywords, ',', 1)) AS product_title,
         title,
         REGEXP_REPLACE(
             REGEXP_EXTRACT(title, '^(.*?)(?:\s*\d*)?\s*(?:Books|E-Books|\(Mg\)|\(LN\))', 1),
-            '\s*เล่ม\s*', 
+            '\s*เล่ม[\s\d\-จบ\(\)\.\,]*',
             '', 
             'g'
         ) AS extracted_series_name,
@@ -40,5 +40,54 @@ extracted AS (
         scrape_at,
         url
     FROM staging
+),
+clean_title_2 AS (
+    SELECT
+        product_id,
+        product_title,
+        title,
+        TRIM(
+            REGEXP_REPLACE(
+                REGEXP_REPLACE(
+                    -- Step 1: Extract the part before 'Books|E-Books|...'
+                    REGEXP_EXTRACT(title, '^(.*?)(?:\s*\d*)?\s*(?:Books|E-Books|\(Mg\)|\(LN\))', 1),
+                    -- Step 2: Remove Prefixes (SET, BOOKSET, ชุด) at the start
+                    '^(SET|BOOKSET|ชุด)\s*', '', 'i'
+                ),
+                -- Step 3: THE TRUNCATOR
+                -- Remove everything starting from:
+                -- A space followed by a number (\s+\d+)
+                -- OR a number followed by a hyphen (\d+-)
+                -- OR a parenthesis containing 'เล่ม' or 'จบ' (\(.*\))
+                -- OR the word 'เล่ม' or 'จบ'
+                '(\s+\d+|\d+-|\s*\(.*(เล่ม|จบ).*\)|เล่ม|จบ).*$', 
+                '', 
+                'g'
+            )
+        ) AS series_title,
+        
+        raw_series_number,
+        is_bundle,
+        is_finished,
+        price,
+        barcode,
+        release_date,
+        keywords,
+        author,
+        publisher,
+        category_lv1,
+        category_lv2,
+        average_rating,
+        rating_count,
+        number_of_page,
+        width_cm,
+        height_cm,
+        thickness_cm,
+        weight_kg,
+        file_size_mb,
+        scrape_at,
+        url
+    FROM clean_title_1
 )
-SELECT * FROM extracted WHERE title LIKE 'ชุด %'
+SELECT * FROM clean_title_2
+WHERE title LIKE 'ชุด%'
